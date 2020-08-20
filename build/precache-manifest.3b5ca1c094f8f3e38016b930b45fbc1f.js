9be22ce0cb291f0f7b82789a392c33db48241432
jtoivo <jiitee1979@gmail.com>
2020-08-20 13:53:59 +0300

    /build added

---
+++ build/precache-manifest.3b5ca1c094f8f3e38016b930b45fbc1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec3907ee6f5a3dcb6a92a58aa2c49e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5d5ff9c453cdbfbc93d",
+    "url": "/static/css/main.9adc0e0d.chunk.css"
+  },
+  {
+    "revision": "77ed0c26fb7a102b89fb",
+    "url": "/static/js/2.936a527d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.936a527d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d5ff9c453cdbfbc93d",
+    "url": "/static/js/main.8c3843c5.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
